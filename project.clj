e1771f049542dd44d3447f781d09cbba3517d7a6
Tolga Paksoy <tolga@hoppinger.com>
2015-12-03 23:24:49 +0100

    Add a test build setup

--- project.clj
+++ project.clj
@@ -12,13 +12,16 @@
                  [secretary "1.2.3"]
                  [venantius/accountant "0.1.5"]
                  [cljs-http "0.1.38"]
-                 [org.omcljs/om "0.9.0"]]
+                 [org.omcljs/om "0.9.0"]
+                 [prismatic/dommy "1.1.0"]]
 
   :plugins [[lein-cljsbuild "1.1.1"]
             [lein-figwheel "0.5.0-1"]
-            [lein-environ "1.0.1"]]
+            [lein-environ "1.0.1"]
+            [lein-doo "0.1.5-SNAPSHOT"]]
 
   :source-paths ["src"]
+  :test-paths ["test"]
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
@@ -41,7 +44,12 @@
                 :compiler {:output-to "resources/public/js/compiled/tarq_cljs.js"
                            :main tarq-cljs.core
                            :optimizations :advanced
-                           :pretty-print false}}]}
+                           :pretty-print false}}
+               {:id "test"
+                :source-paths ["src" "test"]
+                :compiler {:output-to "resources/test/js/compiled/testable.js"
+                           :main tarq-cljs.runner
+                           :optimizations :none}}]}
 
   :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
              ;; :server-port 3449 ;; default
@@ -49,27 +57,9 @@
 
              :css-dirs ["resources/public/css"] ;; watch and update CSS
 
-             ;; Start an nREPL server into the running figwheel process
-             ;; :nrepl-port 7888
-
              ;; Server Ring Handler (optional)
              ;; if you want to embed a ring handler into the figwheel http-kit
              ;; server, this is for simple ring servers, if this
              ;; doesn't work for you just run your own server :)
              ;; :ring-handler hello_world.server/handler
-
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
-
-             ;; if you want to disable the REPL
-             ;; :repl false
-
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
              })
