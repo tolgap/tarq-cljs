55d5db369ec1c68b50e70867d66ae3baebfc0b4d
Tolga Paksoy <tolga@hoppinger.com>
2015-12-06 00:31:08 +0100

    Add test for website list item url

--- test/tarq_cljs/website_test.cljs
+++ test/tarq_cljs/website_test.cljs
@@ -1,16 +1,27 @@
 (ns tarq-cljs.website-test
   (:require [tarq-cljs.components.website :as website]
+            [tarq-cljs.api :as api]
             [tarq-cljs.common-test :refer [append-container!]]
             [dommy.core :as dommy :refer-macros [sel1]]
             [om.core :as om :include-macros true]
             [cljs.test :refer-macros [deftest is testing]]))
 
 (deftest website-list-item
-  (testing "website list component"
+  (testing "website name"
     (is (= "some website name"
            (let [c (append-container!)
                  data {:name "some website name"
                        :server_id 4
                        :id 19}]
              (om/root website/website-list-item data {:target c})
-             (dommy/text (sel1 c :li.collection-item)))))))
+             (dommy/text (sel1 c :li.collection-item))))))
+  (testing "website url"
+    (is
+     (not
+      (empty?
+       (let [c (append-container!)
+             data {:name "website 2"
+                   :server_id 5
+                   :id 1}]
+         (om/root website/website-list-item data {:target c})
+         (str (dommy/attr (sel1 c :a) :href))))))))
