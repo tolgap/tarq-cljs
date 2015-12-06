37cec02a33843bde6d20be1a1bb794cfa6d51b3e
Tolga Paksoy <tolga@hoppinger.com>
2015-12-06 01:19:18 +0100

    Add spec for website list

--- test/tarq_cljs/website_test.cljs
+++ test/tarq_cljs/website_test.cljs
@@ -2,7 +2,7 @@
   (:require [tarq-cljs.components.website :as website]
             [tarq-cljs.api :as api]
             [tarq-cljs.common-test :refer [append-container!]]
-            [dommy.core :as dommy :refer-macros [sel1]]
+            [dommy.core :as dommy :refer-macros [sel1 sel]]
             [om.core :as om :include-macros true]
             [cljs.test :refer-macros [deftest is testing]]))
 
@@ -25,3 +25,12 @@
                    :id 1}]
          (om/root website/website-list-item data {:target c})
          (str (dommy/attr (sel1 c :a) :href))))))))
+
+(deftest website-list
+  (testing "show 2 websites"
+    (is (= 2
+           (let [c (append-container!)
+                 data [{:name "website 1" :server_id 1 :id 1}
+                       {:name "website 2" :server_id 2 :id 20}]]
+             (om/root website/websites-list data {:target c})
+             (count (sel c :li.collection-item)))))))
