45b28b373846e9d90e9e0613c21c32d9826ae89f
Tolga Paksoy <tolga@hoppinger.com>
2016-01-17 13:25:17 +0100

    [WIP] try to write an async test for plugin vulnerability count

--- test/tarq_cljs/plugin_test.cljs
+++ test/tarq_cljs/plugin_test.cljs
@@ -1,9 +1,11 @@
 (ns tarq-cljs.plugin-test
   (:require [tarq-cljs.components.plugin :as plugin]
             [tarq-cljs.common-test :refer [append-container!]]
+            [tarq-cljs.api :as api]
+            [cljs.core.async :refer [put! chan]]
             [dommy.core :as dommy :refer-macros [sel1 sel]]
             [om.core :as om :include-macros true]
-            [cljs.test :refer-macros [deftest is testing]]))
+            [cljs.test :refer-macros [deftest is testing async]]))
 
 (deftest plugin-list-item
   (testing "shows name"
@@ -28,3 +30,20 @@
                        {:name "plugin-2"}]]
              (om/root plugin/plugins-list data {:target c :init-state {:expanded? true}})
              (count (sel c :li.collection-item.plugin)))))))
+
+(deftest ^:async plugin-table-item
+  (testing "should show name, version and vulns"
+    (async done
+           (with-redefs
+             [api/json-to (fn [path]
+                            (.log js/console "PLEASE")
+                            (let [mock-ch (chan 1)]
+                              (put! mock-ch ["1" "2"])
+                              mock-ch))]
+             (let [c (append-container!)
+                   data {:name "plugin"
+                         :version "0.0.1"}]
+               (om/root plugin/plugin-table-item data {:target c})
+               (is (= "plugin0.0.12"
+                      (dommy/text c)))
+               (done))))))
