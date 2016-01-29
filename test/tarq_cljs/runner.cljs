b5666ed259d903576c0767ea3690a22ba68538cc
Tolga Paksoy <tolga@hoppinger.com>
2016-01-30 00:07:18 +0100

    Add vulnerability table row test

--- test/tarq_cljs/runner.cljs
+++ test/tarq_cljs/runner.cljs
@@ -2,8 +2,10 @@
   (:require  [doo.runner :refer-macros [doo-tests]]
              [tarq-cljs.core-test]
              [tarq-cljs.website-test]
-             [tarq-cljs.plugin-test]))
+             [tarq-cljs.plugin-test]
+             [tarq-cljs.vulnerability-test]))
 
 (doo-tests 'tarq-cljs.core-test
            'tarq-cljs.website-test
-           'tarq-cljs.plugin-test)
+           'tarq-cljs.plugin-test
+           'tarq-cljs.vulnerability-test)
